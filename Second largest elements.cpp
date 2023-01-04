4ddcd263a6a74c4be6341943717f80b5897393a5
rishrish89 <70275962+rishrish89@users.noreply.github.com>
2023-01-04 13:35:37 +0530

    Update Second largest elements.cpp

--- Second largest elements.cpp
+++ Second largest elements.cpp
@@ -22,3 +22,30 @@ int main(){
     
     return 0;
 }
+###############################################
+METHOD -2:
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[] = {2,111,5,6,87};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    
+    
+    int max1,max2;
+    max1 = max2 = INT_MIN;
+    for(int i =0;i<n;i++){
+        if(arr[i]>max1){
+            max2 = max1;
+            max1 = arr[i];
+            
+        }
+        else if(arr[i]<max1 && arr[i]>max2){
+            max2 = arr[i];
+        }
+        
+    }
+    cout<<max1<<" "<<max2;
+    
+    
+    
+}
